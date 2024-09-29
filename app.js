57dee622f71d874a0dee605edb489bf721c5974b
Lương Tư Nhân <166272042+ltnhan04@users.noreply.github.com>
2024-09-29 16:15:07 +0700

    Update app.js

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ app.use(cors());
 
 app.use("/api/auth", authRoutes);
 
-app.get("/", () => {
-  console.log("Hello World");
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
