e5e56f6ba1bbaac2ba5e5e9983cc5f1715cfca84
Luong Tu Nhan <tunhan281204@gmail.com>
2024-10-10 00:58:54 +0700

    feat: deploy on vercel

--- controllers/admin/review.controller.js
+++ controllers/admin/review.controller.js
@@ -1,5 +1,5 @@
-const Review = require("../models/reviews.model");
-const Product = require("../models/product.model");
+const Review = require("../../models/reviews.model");
+const Product = require("../../models/product.model");
 const deleteReview = async (req, res) => {
   try {
     const { id } = req.params;
