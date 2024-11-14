e912fbdb8117ab57b991f0657d0ad0bd931529c9
Luong Tu Nhan <tunhan281204@gmail.com>
2024-11-14 16:48:29 +0700

    feat: update profile customer

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -241,6 +241,37 @@ const getProfile = async (req, res) => {
   }
 };
 
+const updateProfile = async (req, res) => {
+  const { name, phoneNumber, address } = req.body;
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.user._id,
+      {
+        name,
+        phoneNumber,
+        address,
+      },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json({
+      message: "Profile updated successfully",
+      user: updatedUser,
+    });
+  } catch (error) {
+    console.log("Error in updateProfile controller:", error.message);
+    res.status(500).json({ message: "Server Error!", error: error.message });
+  }
+};
+
 const forgotPassword = async (req, res) => {
   const { email } = req.body;
   try {
@@ -322,6 +353,7 @@ module.exports = {
   refreshToken,
   verifySignUp,
   getProfile,
+  updateProfile,
   forgotPassword,
   resetPassword,
   resentOTP,
